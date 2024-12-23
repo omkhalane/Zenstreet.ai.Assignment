7a971d841451e542aadb424d0eb5c383e0444446
Om Khalane <om.j.khalane@gmail.com>
2024-12-23 13:14:37 +0530

    Update forms

---
+++ pages/forms/step3.js
@@ -0,0 +1,13 @@
+export default function Step3() {
+  return (
+    <div>
+      <h2>Step 3: Preferences</h2>
+      <form>
+        <label>
+          Preference 1:
+          <input type="checkbox" className="mr-2" />
+        </label>
+      </form>
+    </div>
+  );
+}
