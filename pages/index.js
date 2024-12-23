4b6157e6418092e5cbc4e8780bf22070b81c2188
Om Khalane <om.j.khalane@gmail.com>
2024-12-23 13:52:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/form/step1');
+    router.push('pages/form/step1');
   }, []);
 
   return null;
