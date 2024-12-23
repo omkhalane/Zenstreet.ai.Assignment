c55141880556186652a2051db921d76a8f289885
Om Khalane <om.j.khalane@gmail.com>
2024-12-23 14:18:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  output: 'export', // This enables static HTML export
-  basePath: '/<multi-step-form>', // Replace <repository-name> with your GitHub repository name
-  assetPrefix: '/<multi-step-form>',
+  output: 'export',
+  basePath: '/multi-step-form',
+  assetPrefix: '/multi-step-form',
 };
