e6273e758944a5014dde6bf910838bf98795d661
Om Khalane <om.j.khalane@gmail.com>
2024-12-23 13:14:14 +0530

    Update forms

---
+++ pages/forms/step2.js
@@ -0,0 +1,13 @@
+export default function Step2() {
+  return (
+    <div>
+      <h2>Step 2: Address Details</h2>
+      <form>
+        <label>
+          Address:
+          <input type="text" className="border p-2" />
+        </label>
+      </form>
+    </div>
+  );
+}
