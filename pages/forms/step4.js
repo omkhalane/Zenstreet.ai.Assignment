4cf9b3b9fde1824b33ea5d221aff6f8555ad59e9
Om Khalane <om.j.khalane@gmail.com>
2024-12-23 13:14:52 +0530

    Update forms

---
+++ pages/forms/step4.js
@@ -0,0 +1,9 @@
+export default function Step4() {
+  return (
+    <div>
+      <h2>Step 4: Review & Submit</h2>
+      <p>Review your information before submitting.</p>
+      <button className="bg-blue-500 text-white p-2">Submit</button>
+    </div>
+  );
+}
